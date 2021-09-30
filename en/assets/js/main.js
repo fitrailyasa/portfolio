8756d136ea3e22e5ea6291c0e0780e0b1b71ab7a
fitrailyasa <nde.joker12@gmail.com>
2021-09-30 19:36:23 +0700

    tambah bahasa inggris

--- en/assets/js/main.js
+++ en/assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU TAMPILAN =====*/ 
+/*===== SHOW MENU =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,21 +11,21 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*===== MENU AKTIF DAN TIDAK AKTIF =====*/
+/*===== ACTIVE & DEACTIVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
-  /*Tautan Aktif*/
+  /*Active*/
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
   
-  /*Tidak Aktif*/
+  /*Deactive*/
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== ANIMASI BERGERAK =====*/
+/*===== SCROLL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -33,27 +33,27 @@ const sr = ScrollReveal({
     reset: true
 });
 
-/*GESER MENU UTAMA*/
+/*Home Scroll*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
-/*GESER TENTANG*/
+/*About Scroll*/
 sr.reveal('.about__img',{}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400}); 
 
-/*GESER KEAHLIAN*/
+/*Skill Scroll*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
 sr.reveal('.skills__data',{interval: 200}); 
 sr.reveal('.skills__img',{delay: 600});
 
-/*GESER PROJEK*/
+/*Project Scroll*/
 sr.reveal('.project__img',{interval: 200}); 
 
-/*GESER KONTAK*/
+/*Contact Scroll*/
 sr.reveal('.contact__input',{interval: 200}); 
 
 
